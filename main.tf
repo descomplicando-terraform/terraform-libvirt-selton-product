28ef6d5992b8049bc6301fa9a070d533147dcfee
seltonlucas <sl1586321@gmail.com>
2024-10-02 23:47:40 +0000

    Using tag

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "edge" {
-  source = "git@github.com:descomplicando-terraform/terraform-libvirt-selton-kubernetes.git"
+  source = "git@github.com:descomplicando-terraform/terraform-libvirt-selton-kubernetes.git?ref=0.0.1"
 }
 
 module "controlplane" {
-  source            = "git@github.com:descomplicando-terraform/terraform-libvirt-selton-kubernetes.git"
+  source            = "git@github.com:descomplicando-terraform/terraform-libvirt-selton-kubernetes.git?ref=0.0.1"
   nodes             = ["cp01"]
   memory            = 6144
   libvirt_disk_path = "var/lib/libvirt/pool/cp"
