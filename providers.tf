e35ff4cb9e7293f53e4af02f21eede8d30d3921f
Selton <sl1586321@gmail.com>
2025-06-24 22:59:21 -0300

    provider required and uri

--- providers.tf
+++ providers.tf
@@ -1,3 +1,13 @@
 terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.8.3"
+    }
+  }
   required_version = ">= 1.9.0"
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
 }
